a3201e260b7ee2d357a9003c37e88a2a9a8dd13c
Mustafa Panjiwala <56435815+mustafapanjiwala@users.noreply.github.com>
2021-06-22 12:39:40 +0530

    first commit

---
+++ Assignment-1/leaderRouter.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const leaderRouter = express.Router();
+
+leaderRouter.use(bodyParser.json());
+
+leaderRouter.route('/')
+.all((req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+.get((req,res,next)=>{
+    res.end('WIll send all the leader to you!');
+})
+.post((req,res,next)=>{
+    res.end('WIll add the leader: '+req.body.name+' with details'+req.body.description);
+})
+.put((req,res,next)=>{
+    res.statusCode = 403;
+    res.end('PUT operation not supported on /leader');
+})
+.delete((req,res,next)=>{
+    res.end('Deleting all the leader!');
+});
+
+leaderRouter.route('/:leaderId')
+.all((req,res,next)=>{
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/plain');
+    next();
+})
+.get((req,res,next)=>{
+    res.end('Will send details of the leader'+req.params.leaderId+'to you!');
+})
+.post((req,res,next)=>{
+    res.end('Will add the leader: '+req.body.name+' with details'+req.body.description);
+})
+.put((req,res,next)=>{
+    res.write('Updating the leader:'+req.params.leaderId+'\n');
+    res.end('Will update the leader'+req.body.name+' with details: '+req.body.description);
+})
+.delete((req,res,next)=>{
+    res.end('Deleting the leadere! '+req.params.leaderId);
+});
+
+module.exports = leaderRouter;
